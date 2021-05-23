75b41bb23a1e050518a673f7d728a3a2b0c54b46
Karolina Garaeva <nutvismunt@gmail.com>
2021-05-23 11:22:31 +0300

    fixed patronymic bug

--- DataCompany/Models/Worker.cs
+++ DataCompany/Models/Worker.cs
@@ -17,6 +17,9 @@ namespace DataCompany.Models
         /// </summary>
         public string Surname { get; set; }
         
+        /// <summary>
+        /// worker's patronymic
+        /// </summary>
         public string Patronymic { get; set; }
 
         /// <summary>
